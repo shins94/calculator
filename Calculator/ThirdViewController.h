52fae6b2c121a2ef572ab6f2720648926974b39c
F839-006 M <mf839-006@70-12-242-108.pools.spcsdns.net>
2016-06-17 13:58:38 +0900

    프로퍼티 방법 추가

--- Calculator/ThirdViewController.h
+++ Calculator/ThirdViewController.h
@@ -14,6 +14,9 @@
     void(^changeMainBackColor)(UIColor*);
 }
 
+
+@property void(^newChangeMainBackColor)(UIColor*);
+
 - (IBAction)changeColor:(id)sender;
 - (void) changeColorFunc:(void(^)(UIColor*))myFunc;
 @end
